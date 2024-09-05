9126b48a31b82e00c16fd0f31060fb8bfcf0cde0
RINcHI <rinchi@MacBook-Pro-RINcHI.local>
2024-09-05 14:06:56 +0300

    Third commit

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/RINcHIlol/rest.git/pkg/handler"
 	"github.com/RINcHIlol/rest.git/pkg/repository"
 	"github.com/RINcHIlol/rest.git/pkg/service"
+	_ "github.com/golang-migrate/migrate"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatal(err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(restApi.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
